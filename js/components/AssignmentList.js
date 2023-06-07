a95e21b30ca9e4feb157af2173fe1598300ee534
Rafael Gomes <gomes.webads@gmail.com>
2023-06-07 17:21:29 -0300

    first commit

---
+++ js/components/AssignmentList.js
@@ -0,0 +1,47 @@
+import Assignment from "./Assignment.js"
+import AssignmentTags from "./AssignmentTags.js";
+export default {
+    components: { Assignment, AssignmentTags },
+    template: `
+    <section v-show="assignments.length" class="w-60">
+        <h2 class="font-bold mb-2">
+            {{ title }}
+            <span>({{ assignments.length }})</span>
+        </h2>
+
+        <assignment-tags
+            v-model:currentTag="currentTag"
+            :initial-tags="assignments.map(a => a.tag)"
+        />
+
+        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+            <assignment
+                v-for="assignment in filteredAssignments"
+                :key="assignment.id"
+                :assignment="assignment"
+            ></assignment>
+        </ul>
+        <slot></slot>
+    </section>
+    `,
+
+    props: {
+        assignments: Array,
+        title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all'
+        }
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+    }
+}
